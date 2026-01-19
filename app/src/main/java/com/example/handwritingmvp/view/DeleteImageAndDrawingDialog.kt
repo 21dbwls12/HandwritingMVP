138a80322c3efaa37f7b48903a9ec80161c2ceda
bbangiee <139525941+21dbwls12@users.noreply.github.com>
2026-01-19 17:19:33 +0900

    :recycle: View 관련 파일
    - ScaffoldLayout.kt -> MainScreenLayout.kt
      - layout 패키지 -> view 패키지
    - ScaffoldTopBar.kt 생성
      - Scaffold 요소 관련 함수 정리
    - DeleteImageAndDrawingDialog.kt 생성
      - 휴지통 버튼 클릭시 표시하는 대화상자 관련 함수 정리

---
+++ app/src/main/java/com/example/handwritingmvp/view/DeleteImageAndDrawingDialog.kt
@@ -0,0 +1,33 @@
+package com.example.handwritingmvp.view
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Info
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+
+// 휴지통 버튼을 누르면 나오는 대화상자
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DeleteImageAndDrawingDialog() {
+    AlertDialog(
+        onDismissRequest = {},
+        confirmButton = {
+            // 이미지와 필기 모두 삭제
+            DialogButton("예") {}
+            // 필기만 삭제
+            DialogButton("아니오") { }
+        },
+        icon = { Icon(imageVector = Icons.Rounded.Info, contentDescription = null) },
+        text = { Text("이미지도 같이 삭제하시겠습니까?") }
+    )
+}
+
+// 휴지통 버튼을 누르면 표시되는 대화상자 내부 텍스트 버튼 양식
+@Composable
+private fun DialogButton(dialog: String, onClick: () -> Unit) {
+    TextButton(onClick = onClick) { Text((dialog)) }
+}
---
+++ app/src/main/java/com/example/handwritingmvp/view/ScaffoldTopBar.kt
@@ -0,0 +1,36 @@
+package com.example.handwritingmvp.view
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Check
+import androidx.compose.material.icons.rounded.Delete
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.ImageVector
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ScaffoldTopAppBar() {
+    TopAppBar(
+        title = { Text("필기를 작성하세요") },
+        actions = {
+            // 저장 기능(캡처??)
+            TopBarIconButtons(Icons.Rounded.Check) { }
+            // 삭제 기능(필기, 필기와 이미지)
+            TopBarIconButtons(Icons.Rounded.Delete) { }
+        }
+    )
+}
+
+// 상단에 표시되는 아이콘 버튼 양식
+@Composable
+private fun TopBarIconButtons(icon: ImageVector, onClick: () -> Unit) {
+    IconButton(
+        onClick = onClick
+    ) {
+        Icon(imageVector = icon, contentDescription = null)
+    }
+}
