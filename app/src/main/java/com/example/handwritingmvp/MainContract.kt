5418d64dffe402ad2a62f4afd94db20fc5638f3f
bbangiee <139525941+21dbwls12@users.noreply.github.com>
2026-01-19 17:51:16 +0900

    :art: presenter와 view를 interface로 연결
    - view(MainActivity)와 직접 연결되어 있어서 수정
      - 파라미터에 MainActicity -> MainContract.View로 수정

--- app/src/main/java/com/example/handwritingmvp/MainContract.kt
+++ app/src/main/java/com/example/handwritingmvp/MainContract.kt
@@ -2,10 +2,13 @@ package com.example.handwritingmvp
 
 interface MainContract {
     interface View {
-
+//        fun showDeleteDialog()
+//        fun hideDeleteDialog()
+//        fun updateImage()
     }
 
     interface Presenter {
-
+        fun onDeleteClicked()
+        fun onConfirmDelete(withImage: Boolean)
     }
 }
