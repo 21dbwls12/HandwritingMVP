70b9cf6ef5fb9d88a9f0524c6bc9f9c6bfe9d860
bbangiee <139525941+21dbwls12@users.noreply.github.com>
2026-01-19 17:45:03 +0900

    :hammer: Presenter 구현 클래스 생성
    - MainPresenter.kt 추가

---
+++ app/src/main/java/com/example/handwritingmvp/presenter/MainPresenter.kt
@@ -0,0 +1,7 @@
+package com.example.handwritingmvp.presenter
+
+import com.example.handwritingmvp.MainActivity
+import com.example.handwritingmvp.MainContract
+
+class MainPresenter(view: MainActivity) : MainContract.Presenter {
+}
