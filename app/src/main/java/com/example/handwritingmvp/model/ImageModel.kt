a019fe96ed2916b56b1f8bb601f876fba3ad6d7e
bbangiee <139525941+21dbwls12@users.noreply.github.com>
2026-01-21 17:53:11 +0900

    :art: model 추가
    - 사진 데이터 저장
    
    Fixes: #3

---
+++ app/src/main/java/com/example/handwritingmvp/model/ImageModel.kt
@@ -0,0 +1,23 @@
+package com.example.handwritingmvp.model
+
+import android.net.Uri
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+
+class ImageModel {
+    // 저장된 사진 Uri
+    var savedUri by mutableStateOf< Uri?>(null)
+        // uri값이 외부에서 수정하지 못하도록 setter로 설정
+        private set
+
+    // 저장된 사진 uri를 변경하여 저장
+    fun updateUri(uri:Uri) {
+        savedUri = uri
+    }
+
+    // 저장된 사진 uri 삭제
+    fun deleteUri() {
+        savedUri = null
+    }
+}
